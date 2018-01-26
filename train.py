c57ff87424cf47782cf3a9af54e370fd75750c8d
Lukas Müller <lukas.s.mueller1992@gmail.com>
2018-01-26 15:19:23 +0100

    data sets are now created randomly

--- train.py
+++ train.py
@@ -21,6 +21,7 @@ from custom_vgg16 import *
 
 LOGDIR = "log_tb/"
 
+# Parse command line arguments
 parser = argparse.ArgumentParser(description='Real-time style transfer')
 parser.add_argument('--gpu', '-g', default=-1, type=int,
                     help='GPU ID (negative value indicates CPU)')
@@ -47,33 +48,49 @@ args = parser.parse_args()
 
 data_dict = loadWeightsData('./vgg16.npy')
 batchsize = args.batchsize
-
 n_epoch = args.epoch
 output = args.output
 
 
+# Read in all image paths from given dataset
 fpath_inp = os.listdir(args.dataset)
 fpath_trg = os.listdir(args.targetset)
-imagepaths = []
+# Size of total dataset and train, validation and test sets
+n_data = int(len(fpath_inp))
+n_data = 10
+n_train = int(np.floor(n_data*0.64))
+n_val = int(np.ceil(n_data*0.16))
+n_test = int(n_data*0.2)
+print(n_train, n_val+n_train)
+
+#print(n_data)
+indices = np.arange(n_data)
+np.random.shuffle(indices)
+#ind_train = indices[0:]
+#ind_test = indices[]
+iTrain, iVal, iTest = np.split(indices,[n_train, n_train+n_val])
+all_images = fpath_inp
+random.shuffle(fpath_inp)
+inputpaths = []
 targetpaths = []
-for fn, fn_ in zip(fpath_inp, fpath_trg):
+# Sort paths of images into train, validation and test sets
+# split into TRAINING{80%} [train (80%) &validation (20%)] and TEST DATA{20%}
+# i.e. total data 10: 6 training, 2 validation, 2 test
+for fn in fpath_inp:
     base, ext = os.path.splitext(fn)
-    base_, ext_ = os.path.splitext(fn_)
-    # TODO: check that input and target filenames are similar
     if ext == '.jpg' or ext == '.png':
-        imagepath = os.path.join(args.dataset,fn)
-        imagepaths.append(imagepath)
-    if ext_ == '.jpg' or ext_ == '.png':
-        targetpath = os.path.join(args.targetset,fn_)
-        targetpaths.append(targetpath)
-#split into TRAINING{80%} [train (80%) &validation (20%)] and TEST DATA{20%}
-#i.e. total data 10: 6 training, 2 validation, 2 test
-n_train_data = int(len(imagepaths)*0.64)
-#n_test_data = int(len(imagepaths)*)
-print(n_train_data)
+       imagepath = os.path.join(args.dataset,fn)
+       inputpaths.append(imagepath)
+       targetpath = os.path.join(args.targetset,fn)
+       targetpaths.append(targetpath)
+trainset, valset, testset = np.split(inputpaths,[n_train, n_train+n_val])
+trainset_, valset_, testset_ = np.split(targetpaths,[n_train, n_train+n_val])
 #n_data = len(imagepaths)
-print ('num training images:', n_train_data)
-n_iter = int(n_train_data / batchsize)
+print ('Input images:', n_data)
+print ('Training images:', n_train)
+print ('Validation images:', n_val)
+print ('Test images:', n_test)
+n_iter = int(n_train / batchsize)
 print (n_iter, 'iterations,', n_epoch, 'epochs')
 
 
@@ -106,7 +123,7 @@ with tf.name_scope("loss_network"):
     with tf.name_scope("vgg16_on_output"):
         vgg = custom_Vgg16(outputs, data_dict=data_dict)
         feature = [vgg.conv1_2, vgg.conv2_2, vgg.conv3_3, vgg.conv4_3, vgg.conv5_3]
-    
+
 
     # compute initial loss of input data
     loss_i = tf.zeros(batchsize, tf.float32)
@@ -121,17 +138,10 @@ with tf.name_scope("loss_network"):
         loss_f += tf.reduce_mean(tf.subtract(f, f_) ** 2, [1, 2, 3])
 
     loss = loss_f
-    #loss = tf.reduce_mean(loss_f)
-    #tf.summary.scalar("loss", loss[0])
-    #tf.summary.scalar("loss", tf.reduce_mean(loss))
 
-    # optimizer
+# optimizer
 with tf.name_scope("train"):
     train_step = tf.train.AdamOptimizer(args.lr).minimize(loss)
-
-
-    # merge all the summaries
-    #summary = tf.summary.merge_all()
 
     # for calculating time
     s_time = time.time()
@@ -159,24 +169,6 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_plac
 
     writer = tf.summary.FileWriter(LOGDIR + log_title)
 
-    ## Compute loss of obfuscated images
-    #imgs = np.zeros((batchsize, 112, 112, 3), dtype=np.float32)
-    #trgs = np.zeros((batchsize, 112, 112, 3), dtype=np.float32)
-    #initLoss = 0
-    #for i in range(n_iter):
-    #    for j in range(batchsize):
-    #        p = imagepaths[i*batchsize + j]
-    #        q = targetpaths[i*batchsize + j]
-    #        imgs[j] = np.asarray(Image.open(p).convert('RGB').resize((112, 112)), np.float32)
-    #        trgs[j] = np.asarray(Image.open(q).convert('RGB').resize((112, 112)), np.float32)
-    #    feed_dict = {inputs: imgs, target:trgs}
-    #    inLoss = sess.run([megaloss], feed_dict=feed_dict)
-    #    print(inLoss)
-    #    initLoss += initLoss_
-    #initLoss = np.sum(initLoss) / n_data
-    #print("Initial average loss of obfuscated data: ", initialLoss)
-
-
     for epoch in range(n_epoch):
         print ('epoch', epoch)
         imgs = np.zeros((batchsize, 112, 112, 3), dtype=np.float32)
@@ -186,11 +178,12 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_plac
         loss_total = 0
         iLoss_total = 0
         s_total = 0
+        # TRAINING
         for i in range(n_iter):
             #reading in all the images into the batch
             for j in range(batchsize):
-                p = imagepaths[i*batchsize + j]
-                q = targetpaths[i*batchsize + j]
+                p = trainset[i*batchsize + j]
+                q = trainset_[i*batchsize + j]
                 imgs[j] = np.asarray(Image.open(p).convert('RGB').resize((112, 112)), np.float32)
                 trgs[j] = np.asarray(Image.open(q).convert('RGB').resize((112, 112)), np.float32)
             feed_dict = {inputs: imgs, target:trgs}
@@ -204,13 +197,9 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_plac
         summary.value.add(tag="Loss_Training", simple_value=loss_total)
         writer.add_summary(summary, epoch)
         #check validation loss at end of each epoch (using one batch of validation images)
-        #TO DO: change the randomized choice of images in validation set
-        r = random.randint(0,2)
-        #print(r)
-        for j in range(batchsize):
-            p = imagepaths[n_train_data+r+j]
-            q = targetpaths[n_train_data+r+j]
-            #print(p,q)
+        for j in range(n_val):
+            p = valset[j]
+            q = valset_[j]
             imgs_val[j] = np.asarray(Image.open(p).convert('RGB').resize((112, 112)), np.float32)
             trgs_val[j] = np.asarray(Image.open(q).convert('RGB').resize((112, 112)), np.float32)
         feed_dict = {inputs: imgs_val, target:trgs_val}
@@ -225,27 +214,11 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_plac
     p = imagepaths[rand_index]
     test_image = np.asarray(Image.open(p).convert('RGB').resize((112, 112)), np.float32)
     test_image =tf. convert_to_tensor(test_image)
-    #output = model(test_image) MAKING PROBLEMS
-    #tf.summary.image('Output' , output, 3)
+    output = model(test_image) #MAKING PROBLEMS
+    tf.summary.image('Output' , output, 3)
 
     savepath = saver.save(sess, model_directory + args.output + '.ckpt')
     print('Saved the model to ', savepath)
 
     for var in tf.global_variables():
         var_list[var.name] = var.eval()
-
-
-#Model = tf.Graph()
-#with Model.as_default():
-#    #with tf.device(device_):
-#    inputs = tf.placeholder(tf.float32, shape=[1, 112, 112, 3], name='input')
-#    # feed dictionary into Transform Net, "train=False" would save all values as constants.
-#    transform = FastStyleNet(train=False, data_dict=var_list)
-#    outputs = transform(inputs)
-#    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)) as sess:
-#
-#        save_path = './graphs/'
-#        if not os.path.exists(save_path):
-#            os.makedirs(save_path)
-#        print('saving pb...')
-#        tf.train.write_graph(sess.graph_def, save_path, args.output + '.pb', as_text=False)
